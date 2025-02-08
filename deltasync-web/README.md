3d7ad0ab989ad9d53321be57a6a14e5f172e8220
Steve NOSSE <nossesteve@gmail.com>
2025-02-08 22:33:52 +0100

    init web

---
+++ deltasync-web/README.md
@@ -0,0 +1,1 @@
+# DeltaSync Front
---
+++ deltasync-web/nuxt.config.ts
@@ -0,0 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  css: ['~/assets/css/main.css'],
+  devtools: { enabled: true },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
