724d0b1a6f1a39d492c1b0ea6cd7f02cd432b927
Steve NOSSE <nossesteve@gmail.com>
2025-02-25 08:34:07 +0100

    robots + sitemap

--- pocketsync-web/nuxt.config.ts
+++ pocketsync-web/nuxt.config.ts
@@ -16,6 +16,21 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiBaseUrl: process.env.API_BASE_URL,
+      OPENPANEL_CLIENT_ID: process.env.NODE_ENV === 'production' ? process.env.OPENPANEL_CLIENT_ID : undefined,
+      OPENPANEL_CLIENT_SECRET: process.env.NODE_ENV === 'production' ? process.env.OPENPANEL_CLIENT_SECRET : undefined,
     }
+  },
+  robots: {
+    UserAgent: '*',
+    Allow: '/',
+    Sitemap: 'https://pocketsync.dev/sitemap.xml'
+  },
+  sitemap: {
+    hostname: process.env.NODE_ENV === 'production' ? 'https://codelia.dev' : 'http://localhost:3000',
+    gzip: true,
+    exclude: [
+      '/auth/**',
+      '/console/**',
+    ]
   },
 });
