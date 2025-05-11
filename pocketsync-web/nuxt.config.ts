8d98035820bd12707819ee97ffb361eef40b8f68
Steve NOSSE <nossesteve@gmail.com>
2025-05-11 10:32:56 +0200

    ditching ssr

--- pocketsync-web/nuxt.config.ts
+++ pocketsync-web/nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
-  ssr: true,
+  ssr: false,
   modules: ['@pinia/nuxt'],
   vite: {
     build: {
